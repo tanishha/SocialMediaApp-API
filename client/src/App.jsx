e9d3d34ab92b81c22bc15ac7617f2eb4ccd061d2
Keshav Jaiswal <keshav.jaiswal39@gmail.com>
2021-08-06 13:40:09 +0530

    Installation of fonts, material icons and cores

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,8 @@
 import React from "react";
-import "./App.css";
+import Home from "./pages/home/Home";
 
 function App() {
-  return <div className="App">This is client side</div>;
+  return <Home/>;
 }
 
 export default App;
