b4bea07307b827d34ad94f5509a16d84a8f6acb4
Keshav Jaiswal <keshav.jaiswal39@gmail.com>
2021-08-06 15:20:59 +0530

    Sidebar components with material icons

--- client/src/components/sidebar/Sidebar.jsx
+++ client/src/components/sidebar/Sidebar.jsx
@@ -1,10 +1,124 @@
 import React from 'react'
 import "./sidebar.css"
+import "@material-ui/icons"
+import { Bookmark, Chat, Event, Group, HelpOutline, PlayCircleFilledOutlined, RssFeed, School, WorkOutline } from '@material-ui/icons'
 
 export default function Sidebar() {
     return (
         <div className="sidebar">
-            sidebar
+            <div className="sidebarWrapper">
+                <ul className="sidebarList">
+                    <li className="sidebarListItem">
+                        <RssFeed className="sidebarIcon"/>
+                        <span className="sidebarListItemText">Feed</span>
+                    </li>
+                    <li className="sidebarListItem">
+                        <Chat className="sidebarIcon"/>
+                        <span className="sidebarListItemText">Chats</span>
+                    </li>
+                    <li className="sidebarListItem">
+                        <PlayCircleFilledOutlined className="sidebarIcon"/>
+                        <span className="sidebarListItemText">Videos</span>
+                    </li>
+                    <li className="sidebarListItem">
+                        <Group className="sidebarIcon"/>
+                        <span className="sidebarListItemText">Groups</span>
+                    </li>
+                    <li className="sidebarListItem">
+                        <Bookmark className="sidebarIcon"/>
+                        <span className="sidebarListItemText">Bookmarks</span>
+                    </li>
+                    <li className="sidebarListItem">
+                        <HelpOutline className="sidebarIcon"/>
+                        <span className="sidebarListItemText">Questions</span>
+                    </li>
+                    <li className="sidebarListItem">
+                        <WorkOutline className="sidebarIcon"/>
+                        <span className="sidebarListItemText">Jobs</span>
+                    </li>
+                    <li className="sidebarListItem">
+                        <Event className="sidebarIcon"/>
+                        <span className="sidebarListItemText">Events</span>
+                    </li>
+                    <li className="sidebarListItem">
+                        <School className="sidebarIcon"/>
+                        <span className="sidebarListItemText">Courses</span>
+                    </li>
+                </ul>
+                <button className="sidebarButton">Show More</button>
+                <hr className="sidebarHr"/>
+                <ul className="sidebarFriendList">
+                    <li className="sidebarFriend">
+                        <img className="sidebarFriendImg" src="/assets/person/2.jpeg" alt="" />
+                        <span className="sidebarFriendName">Daniel Roey</span>
+                    </li>
+                    <li className="sidebarFriend">
+                        <img className="sidebarFriendImg" src="/assets/person/2.jpeg" alt="" />
+                        <span className="sidebarFriendName">Daniel Roey</span>
+                    </li>
+                    <li className="sidebarFriend">
+                        <img className="sidebarFriendImg" src="/assets/person/2.jpeg" alt="" />
+                        <span className="sidebarFriendName">Daniel Roey</span>
+                    </li>
+                    <li className="sidebarFriend">
+                        <img className="sidebarFriendImg" src="/assets/person/2.jpeg" alt="" />
+                        <span className="sidebarFriendName">Daniel Roey</span>
+                    </li>
+                    <li className="sidebarFriend">
+                        <img className="sidebarFriendImg" src="/assets/person/2.jpeg" alt="" />
+                        <span className="sidebarFriendName">Daniel Roey</span>
+                    </li>
+                    <li className="sidebarFriend">
+                        <img className="sidebarFriendImg" src="/assets/person/2.jpeg" alt="" />
+                        <span className="sidebarFriendName">Daniel Roey</span>
+                    </li>
+                    <li className="sidebarFriend">
+                        <img className="sidebarFriendImg" src="/assets/person/2.jpeg" alt="" />
+                        <span className="sidebarFriendName">Daniel Roey</span>
+                    </li>
+                    <li className="sidebarFriend">
+                        <img className="sidebarFriendImg" src="/assets/person/2.jpeg" alt="" />
+                        <span className="sidebarFriendName">Daniel Roey</span>
+                    </li>
+                    <li className="sidebarFriend">
+                        <img className="sidebarFriendImg" src="/assets/person/2.jpeg" alt="" />
+                        <span className="sidebarFriendName">Daniel Roey</span>
+                    </li>
+                    <li className="sidebarFriend">
+                        <img className="sidebarFriendImg" src="/assets/person/2.jpeg" alt="" />
+                        <span className="sidebarFriendName">Daniel Roey</span>
+                    </li>
+                    <li className="sidebarFriend">
+                        <img className="sidebarFriendImg" src="/assets/person/2.jpeg" alt="" />
+                        <span className="sidebarFriendName">Daniel Roey</span>
+                    </li>
+                    <li className="sidebarFriend">
+                        <img className="sidebarFriendImg" src="/assets/person/2.jpeg" alt="" />
+                        <span className="sidebarFriendName">Daniel Roey</span>
+                    </li>
+                    <li className="sidebarFriend">
+                        <img className="sidebarFriendImg" src="/assets/person/2.jpeg" alt="" />
+                        <span className="sidebarFriendName">Daniel Roey</span>
+                    </li>
+                    <li className="sidebarFriend">
+                        <img className="sidebarFriendImg" src="/assets/person/2.jpeg" alt="" />
+                        <span className="sidebarFriendName">Daniel Roey</span>
+                    </li>
+                    <li className="sidebarFriend">
+                        <img className="sidebarFriendImg" src="/assets/person/2.jpeg" alt="" />
+                        <span className="sidebarFriendName">Daniel Roey</span>
+                    </li>
+                    <li className="sidebarFriend">
+                        <img className="sidebarFriendImg" src="/assets/person/2.jpeg" alt="" />
+                        <span className="sidebarFriendName">Daniel Roey</span>
+                    </li>
+                    <li className="sidebarFriend">
+                        <img className="sidebarFriendImg" src="/assets/person/2.jpeg" alt="" />
+                        <span className="sidebarFriendName">Daniel Roey</span>
+                    </li>
+
+                </ul>
+            </div>
         </div>
     )
 }
