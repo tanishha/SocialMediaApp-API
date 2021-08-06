384036d5c6ba1dee704a3525c065b65f9d3d8cd5
Keshav Jaiswal <keshav.jaiswal39@gmail.com>
2021-08-06 20:51:51 +0530

    Profile Page

---
+++ client/src/pages/profile/Profile.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import "./profile.css"
+import Topbar from "../../components/topbar/Topbar";
+import Sidebar from "../../components/sidebar/Sidebar";
+import Feed from "../../components/feed/Feed";
+import Rightbar from "../../components/rightbar/Rightbar";
+
+const Profile = () => {
+    return (
+        <div>
+            <Topbar />
+            <div className="profile">
+                <Sidebar />
+                <div className="profileRight">
+                    <div className="profileRightTop">
+                        <div className="profileCover">
+                            <img src="assets/post/3.jpeg" alt="" className="profileCoverImg" />
+                            <img src="assets/post/7.jpeg" alt="" className="profileUserImg" />
+                        </div>
+                        <div className="profileInfo">
+                            <h4 className="profileInfoName">Safak Kocaoglu</h4>
+                            <span className="profileInfoDesc">Hello my friends</span>
+                        </div>
+                    </div>
+                    <div className="profileRightBottom">
+                        <Feed/>
+                        <Rightbar profile/>
+                    </div>
+                </div>
+                
+
+            </div>
+            
+
+        </div>
+    );
+}
+
+export default Profile;
