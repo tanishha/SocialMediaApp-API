6832d5d5d3f937b39e467bf46ea589273a54be22
Keshav Jaiswal <keshav.jaiswal39@gmail.com>
2021-08-06 17:01:05 +0530

    Rightbar Component

--- client/src/components/rightbar/Rightbar.jsx
+++ client/src/components/rightbar/Rightbar.jsx
@@ -4,7 +4,139 @@ import "./rightbar.css"
 export default function Rightbar() {
     return (
         <div className="rightbar">
-            rightbar
+            <div className="rightbarWrapper">
+                <div className="birthdayContainer">
+                    <img src="/assets/gift.png" alt="" className="birthdayImg" />
+                    <span className="birthdayText">
+                        <b>Rohan</b> and <b>3 other friends</b> have a birthday today.
+                    </span>
+                </div>
+                <img className="rightbarAd" src="/assets/ad.png" alt="" />
+                <h4 className="rightbarTitle">Online Friends</h4>
+                <ul className="rightbarFriendList">
+                <li className="rightbarFriend">
+                        <div className="rightbarProfileImgContainer">
+                            <img src="assets/person/3.jpeg" alt="" className="rightbarProfileImg" />
+                            <span className="rightbarOnline"></span>
+                        </div>
+                        <span className="rightbarUsername">John Phillip</span>
+                    </li><li className="rightbarFriend">
+                        <div className="rightbarProfileImgContainer">
+                            <img src="assets/person/3.jpeg" alt="" className="rightbarProfileImg" />
+                            <span className="rightbarOnline"></span>
+                        </div>
+                        <span className="rightbarUsername">John Phillip</span>
+                    </li><li className="rightbarFriend">
+                        <div className="rightbarProfileImgContainer">
+                            <img src="assets/person/3.jpeg" alt="" className="rightbarProfileImg" />
+                            <span className="rightbarOnline"></span>
+                        </div>
+                        <span className="rightbarUsername">John Phillip</span>
+                    </li><li className="rightbarFriend">
+                        <div className="rightbarProfileImgContainer">
+                            <img src="assets/person/3.jpeg" alt="" className="rightbarProfileImg" />
+                            <span className="rightbarOnline"></span>
+                        </div>
+                        <span className="rightbarUsername">John Phillip</span>
+                    </li><li className="rightbarFriend">
+                        <div className="rightbarProfileImgContainer">
+                            <img src="assets/person/3.jpeg" alt="" className="rightbarProfileImg" />
+                            <span className="rightbarOnline"></span>
+                        </div>
+                        <span className="rightbarUsername">John Phillip</span>
+                    </li><li className="rightbarFriend">
+                        <div className="rightbarProfileImgContainer">
+                            <img src="assets/person/3.jpeg" alt="" className="rightbarProfileImg" />
+                            <span className="rightbarOnline"></span>
+                        </div>
+                        <span className="rightbarUsername">John Phillip</span>
+                    </li><li className="rightbarFriend">
+                        <div className="rightbarProfileImgContainer">
+                            <img src="assets/person/3.jpeg" alt="" className="rightbarProfileImg" />
+                            <span className="rightbarOnline"></span>
+                        </div>
+                        <span className="rightbarUsername">John Phillip</span>
+                    </li><li className="rightbarFriend">
+                        <div className="rightbarProfileImgContainer">
+                            <img src="assets/person/3.jpeg" alt="" className="rightbarProfileImg" />
+                            <span className="rightbarOnline"></span>
+                        </div>
+                        <span className="rightbarUsername">John Phillip</span>
+                    </li><li className="rightbarFriend">
+                        <div className="rightbarProfileImgContainer">
+                            <img src="assets/person/3.jpeg" alt="" className="rightbarProfileImg" />
+                            <span className="rightbarOnline"></span>
+                        </div>
+                        <span className="rightbarUsername">John Phillip</span>
+                    </li><li className="rightbarFriend">
+                        <div className="rightbarProfileImgContainer">
+                            <img src="assets/person/3.jpeg" alt="" className="rightbarProfileImg" />
+                            <span className="rightbarOnline"></span>
+                        </div>
+                        <span className="rightbarUsername">John Phillip</span>
+                    </li><li className="rightbarFriend">
+                        <div className="rightbarProfileImgContainer">
+                            <img src="assets/person/3.jpeg" alt="" className="rightbarProfileImg" />
+                            <span className="rightbarOnline"></span>
+                        </div>
+                        <span className="rightbarUsername">John Phillip</span>
+                    </li><li className="rightbarFriend">
+                        <div className="rightbarProfileImgContainer">
+                            <img src="assets/person/3.jpeg" alt="" className="rightbarProfileImg" />
+                            <span className="rightbarOnline"></span>
+                        </div>
+                        <span className="rightbarUsername">John Phillip</span>
+                    </li><li className="rightbarFriend">
+                        <div className="rightbarProfileImgContainer">
+                            <img src="assets/person/3.jpeg" alt="" className="rightbarProfileImg" />
+                            <span className="rightbarOnline"></span>
+                        </div>
+                        <span className="rightbarUsername">John Phillip</span>
+                    </li><li className="rightbarFriend">
+                        <div className="rightbarProfileImgContainer">
+                            <img src="assets/person/3.jpeg" alt="" className="rightbarProfileImg" />
+                            <span className="rightbarOnline"></span>
+                        </div>
+                        <span className="rightbarUsername">John Phillip</span>
+                    </li><li className="rightbarFriend">
+                        <div className="rightbarProfileImgContainer">
+                            <img src="assets/person/3.jpeg" alt="" className="rightbarProfileImg" />
+                            <span className="rightbarOnline"></span>
+                        </div>
+                        <span className="rightbarUsername">John Phillip</span>
+                    </li><li className="rightbarFriend">
+                        <div className="rightbarProfileImgContainer">
+                            <img src="assets/person/3.jpeg" alt="" className="rightbarProfileImg" />
+                            <span className="rightbarOnline"></span>
+                        </div>
+                        <span className="rightbarUsername">John Phillip</span>
+                    </li><li className="rightbarFriend">
+                        <div className="rightbarProfileImgContainer">
+                            <img src="assets/person/3.jpeg" alt="" className="rightbarProfileImg" />
+                            <span className="rightbarOnline"></span>
+                        </div>
+                        <span className="rightbarUsername">John Phillip</span>
+                    </li><li className="rightbarFriend">
+                        <div className="rightbarProfileImgContainer">
+                            <img src="assets/person/3.jpeg" alt="" className="rightbarProfileImg" />
+                            <span className="rightbarOnline"></span>
+                        </div>
+                        <span className="rightbarUsername">John Phillip</span>
+                    </li><li className="rightbarFriend">
+                        <div className="rightbarProfileImgContainer">
+                            <img src="assets/person/3.jpeg" alt="" className="rightbarProfileImg" />
+                            <span className="rightbarOnline"></span>
+                        </div>
+                        <span className="rightbarUsername">John Phillip</span>
+                    </li><li className="rightbarFriend">
+                        <div className="rightbarProfileImgContainer">
+                            <img src="assets/person/3.jpeg" alt="" className="rightbarProfileImg" />
+                            <span className="rightbarOnline"></span>
+                        </div>
+                        <span className="rightbarUsername">John Phillip</span>
+                    </li>
+                </ul>
+            </div>
         </div>
-    )
+    );
 }
