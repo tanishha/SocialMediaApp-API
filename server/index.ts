9cffb143c6fdc33aa9760104ef28631db19598fc
Tanisha Chaudhary <tanishach203@gmail.com>
2021-08-09 13:22:57 +0545

    error resolved

--- server/index.ts
+++ server/index.ts
@@ -1,5 +1,6 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 const express = require("express"),
-  app = express();
+    app = express();
 const morgan = require("morgan");
 
 // Db
@@ -12,36 +13,54 @@ const apiRoute = require("./routes/api.routes.ts");
 app.use(morgan("dev"));
 
 // Inbuilt middleware for parsing incoming data
-app.use(
-  express.urlencoded({
-    extended: true,
-  })
-);
+app.use(express.urlencoded({
+    // eslint-disable-next-line quote-props
+    extended: true
+}));
 app.use(express.json()); // (for json)
 
 // Load routing level middleware
-app.use('/api', apiRoute)
+app.use(
+    "/api",
+    apiRoute
+);
 
-app.use(function (req, res, next) {
-  //for undefined request
-  next({
-    msg: "NOT FOUND 404",
-    status: 404,
-  });
+app.use((req, res, next) => {
+
+    // For undefined request
+    next({
+        "msg": "NOT FOUND 404",
+        "status": 404
+    });
+
 });
-//error handling middleware
-app.use(function (err, req, res, next) {
-  console.log("Error is >>", err);
-  res.status(err.status || 400).json({
-    msg: err.msg || err,
-    status: err.status || 400,
-  });
+// Error handling middleware
+app.use((err, req, res) => {
+
+    console.log(
+        "Error is >>",
+        err
+    );
+    res.status(err.status || 400).json({
+        "msg": err.msg || err,
+        "status": err.status || 400
+    });
+
 });
 
-app.listen(3000, (err, done) => {
-  if (err) {
-    console.log("Server listening failed");
-  } else {
-    console.log("Server listening at port 3000");
-  }
-});
+app.listen(
+    3000,
+    (err) => {
+
+        if (err) {
+
+            console.log("Server listening failed");
+
+        } else {
+
+            console.log("Server listening at port 3000");
+
+        }
+
+    }
+);
